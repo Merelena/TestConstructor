42454f3b354a3395a5cfc81e8b100032a7ae944b
Elena Fyodorova <47062507+Merelena@users.noreply.github.com>
2020-04-30 16:06:21 +0300

    Adding new subjects to database with message has been added.

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from kivy.config import Config
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from models import check_database
+from models import check_database, add_subject_to_db
+from kivy.uix.popup import Popup
 
 Config.set('graphics', 'width', 800)
 Config.set('graphics', 'height', 500)
 Config.set('graphics', 'resizable', '0')
+
+global subject, start, popup
 
 
 class Start(BoxLayout):
@@ -20,21 +23,42 @@ class Start(BoxLayout):
 
     def button_continue(self):
         """Реакция на нажатие кнопки "Продолжить" стартового окна"""
-        if self.ids.text_input1.text == 'a':
-            check_database()
-        else: print('No')
-
+        flag = check_database(self.ids.text_input1.text)    # Проверка наличия учебного предмета в БД
+        # Если учебного предмета нет в БД, появляется кнопка для добавления
+        if not flag:
+            self.ids.button1.opacity = 1
 
     def button_add_subject(self):
         """Реакция на нажатие кнопки добавления нового учебного предмета"""
-        print('Really OK')
+        global popup
+        popup = AddSubject()
+        popup.open()
 
 
-class StartApp(App):
+class AddSubject(Popup):
+    """Окно добавления нового учебного предмета"""
+    def __init__(self, **kwargs):
+        super(AddSubject, self).__init__(**kwargs)
+
+    def add_subject(self):
+        global subject
+        subject = self.ids.text_input2.text
+        add_subject_to_db(subject)
+        popup.dismiss()
+        start.ids.button1.opacity = 0
+        start.ids.text_input1.text = subject
+        print(subject)
+
+    def delete_text(self):
+        self.ids.text_input2.text = ''
+
+
+class TestConstructorApp(App):
     def build(self):
-        return Start()
+        global start
+        start = Start()
+        return start
 
 
-start = StartApp()
 if __name__ == '__main__':
-    start.run()
+    TestConstructorApp().run()
--- models.py
+++ models.py
@@ -1,9 +1,22 @@
 import sqlite3
 
 
-def check_database():
+def check_database(subject):
     conn = sqlite3.connect('TestConstructorDB.db')
     cursor = conn.cursor()
-    cursor.execute("""""")
+    response = cursor.execute("""
+    SELECT subject_name FROM Subjects
+    """)
+    return subject in response;
+    print('Done')
+    conn.close()
+
+
+def add_subject_to_db(subject):
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    cursor.execute(f"""
+    INSERT INTO Subjects(subject_name) VALUES ('{subject}');
+    """)
     print('Done')
     conn.close()
