5bde187737c9b46792b2e53cf402da25bf323605
Elena Fyodorova <47062507+Merelena@users.noreply.github.com>
2020-05-20 20:28:33 +0300

    New features in interface.

--- models.py
+++ models.py
@@ -36,13 +36,24 @@ def create_test(name):
     conn.close()
 
 
-def save_task(task, level, id_subject, test_name):
+def save_new_task(task, level, id_subject, test_name):
     conn = sqlite3.connect('TestConstructorDB.db')
     cursor = conn.cursor()
     cursor.execute(f"""
             INSERT INTO Tasks(task_text, level, id_subject, id_test) 
 VALUES ('{task}', '{level}', '{id_subject}', '{search_test_id(test_name)}');
             """)
+    print('z')
+    conn.commit()
+    conn.close()
+
+
+def save_task(task, level, id_task):
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    cursor.execute(f"""
+                UPDATE Tasks  SET task_text='{task}', level='{level}' WHERE id_tasks='{id_task}';
+                """)
     print('z')
     conn.commit()
     conn.close()
@@ -80,6 +91,7 @@ def delete_test(test_name):
                        DELETE FROM Tasks WHERE id_test='{search_test_id(test_name)}';
                        """)
     print('Done')
+    conn.commit()
     conn.close()
 
 
@@ -87,9 +99,45 @@ def search_task(task, level, id_subject, test_name):
     conn = sqlite3.connect('TestConstructorDB.db')
     cursor = conn.cursor()
     id_test = search_test_id(test_name)
-    cursor.execute(f"""SELECT id_task FROM Tasks WHERE task_text='{task}' AND level='{level}' AND 
-                        id_subject='{id_subject}' AND id_test={id_test};
-                       """)
-    print('Done')
-    return cursor.fetchall()[0]
+    cursor.execute(f"""SELECT id_tasks FROM Tasks WHERE task_text='{task}' AND level='{level}' 
+AND id_subject='{id_subject}' AND id_test='{id_test}';""")
+    print('Done save task')
+    return cursor.fetchall()[0][0]
+    conn.close()
+
+
+def show_tasks(test_name, id_subject):
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    id_test = search_test_id(test_name)
+    cursor.execute(f"""SELECT id_tasks, level FROM Tasks WHERE id_test='{search_test_id(test_name)}' AND id_subject='{id_subject}';""")
+    print('Done show tasks')
+    return cursor.fetchall()
+    conn.close()
+
+def show_tests():
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        f"""SELECT DISTINCT test_name FROM Tests;""")
+    print('Done show tests')
+    return cursor.fetchall()
+    conn.close()
+
+def open_task(id_task):
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        f"""SELECT task_text, level FROM Tasks WHERE id_tasks='{id_task}';""")
+    print('Done open task')
+    return cursor.fetchall()
+    conn.close()
+
+def delete_task(id_task):
+    conn = sqlite3.connect('TestConstructorDB.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        f"""DELETE FROM Tasks WHERE id_tasks='{id_task}';""")
+    print('Done delete task')
+    return cursor.fetchall()
     conn.close()
